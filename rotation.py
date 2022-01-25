0391e694dc14f193aef117633a98a7059b6c4c15
ssufunny <wltnwhgdkgo@naver.com>
2022-01-25 16:27:02 +0900

    dirNum -> dirMode

--- rotation.py
+++ rotation.py
@@ -3,77 +3,39 @@ from gpio import Motor # 라이브러리 불러오기
 motor = Motor(forward = gpioNum1, backward = gpioNum2) # 라즈베리파이 연결 gpioNum = pin 번호
 
 # speedNum : 개발자가 설정한 회전 속도
-# dirValue : 개발자가 설정한 회전 방향
+# dirMode : 개발자가 설정한 회전 방향
 # count : 개발자가 설정한 회전 수
 # durationTime : 개발자가 설정한 회전 시간
 
-
-
 # 함수로 설정하기 (일단 잔뜩 아닌거같음)
-def forward(speedNum, dirNum, durationTime) : # 정방향 회전
+def forward(speedNum, dirMode, durationTime) : # 정방향 회전
     for _ in range(durationTime) :
         motor.forward(speed=speedNum)
-        if dirNum == 0 :
-            stop(speedNum, dirNum, durationTime)
-        elif dirNum == -1 :
-            backward(speedNum, dirNum, durationTime)
+        if dirMode == 0 :
+            stop(speedNum, dirMode, durationTime)
+        elif dirMode == -1 :
+            backward(speedNum, dirMode, durationTime)
 
-def backward(speedNum, dirNum, durationTime) : # 역방향 회전
+def backward(speedNum, dirMode, durationTime) : # 역방향 회전
     for _ in range(durationTime) :
         motor.forward(speed=speedNum)
-        if dirNum == 0 :
-            stop(speedNum, dirNum, durationTime)
-        elif dirNum == 1 :
-            forward(speedNum, dirNum, durationTime)
+        if dirMode == 0 :
+            stop(speedNum, dirMode, durationTime)
+        elif dirMode == 1 :
+            forward(speedNum, dirMode, durationTime)
 
-def stop(speedNum, dirNum, durationTime) : # 정지
+def stop(speedNum, dirMode, durationTime) : # 정지
     for _ in range(durationTime) :
         motor.forward(speed=speedNum) 
-        if dirNum == 1 :
-            forward(speedNum, dirNum, durationTime)
-        elif dirNum == -1 :
-            backward(speedNum, dirNum, durationTime)
+        if dirMode == 1 :
+            forward(speedNum, dirMode, durationTime)
+        elif dirMode == -1 :
+            backward(speedNum, dirMode, durationTime)
 
-if dirNum == 1 : # 정방향 회전
-    forward(speedNum, dirNum, durationTime)
-elif dirNum == -1 : # 역방향 회전
-    backward(speedNum, dirNum, durationTime)
-elif dirNum == 0 : # 정지
-    stop(speedNum, dirNum, durationTime)
+if dirMode == 1 : # 정방향 회전
+    forward(speedNum, dirMode, durationTime)
+elif dirMode == -1 : # 역방향 회전
+    backward(speedNum, dirMode, durationTime)
+elif dirMode == 0 : # 정지
+    stop(speedNum, dirMode, durationTime)
 
-# durationTime 추가
-if dirNum == 1 : # 정방향 회전
-    for _ in range(durationTime) :
-        motor.forward(speed=speedNum)
-        if dirNum != 0 or dirNum != -1 :
-            exit()
-if dirNum == -1 : # 역방향 회전
-    for _ in range(durationTime) :
-        motor.forward(speed=speedNum)
-if dirNum == 0 : # 정지
-    for _ in range(durationTime) :
-        motor.forward(speed=speedNum)
-
-# count 제거
-if dirValue == 1 : # 정방향 회전 
-    while (dirValue != 0 or dirValue != -1) : # 역방향 회전 or 정지가 아니면 계속 반복
-        motor.forward(speed=speedNum)
-elif dirValue == -1 : # 역방향 회전 
-    while (dirValue != 0 or dirValue != 1) : # 정방향 회전 or 정지가 아니면 계속 반복
-        motor.backward(speed=speedNum)
-elif dirValue == 0 : # 정지
-    exit()
-    
-# 초기
-if dirValue == 1 : # 정방향 회전
-    for i in range(count) :
-        motor.forward(speed=speedNum)
-elif dirValue == -1 : # 역방향 회전
-    for i in count : 
-        motor.backward(speed=speedNum)
-elif dirValue == 0 : # 정지
-    for i in reversed(range(speedNum*10)) :
-        motor.forward(speed=i/10)
-# or
-# elif dirValue == 0 : # 정지
-#     exit()
