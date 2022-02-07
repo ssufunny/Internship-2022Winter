dbf88e3efcfdbff9089996f9f350aa0b60d2b20b
ssufunny <wltnwhgdkgo@naver.com>
2022-02-07 17:24:03 +0900

    upload

---
+++ update.sql
@@ -0,0 +1,9 @@
+-- 온습도계가 아닌 장비의 TempHumiType을 null로 변경
+UPDATE test SET TempHumiType ='' WHERE ObjectType != 'Y' and ObjectType != 'T' and ObjectType != 'H'; 
+
+-- FAN이 아닌 장비의 FanDevDriverType을 null로 변경
+UPDATE test SET FanDevDriverType ='' WHERE ObjectType != 'F';
+
+-- Motor가 아닌 장비의 MotorOperTime을 null로 변경
+UPDATE test SET MotorOperTime ='' WHERE ObjectType != 'M';
+
