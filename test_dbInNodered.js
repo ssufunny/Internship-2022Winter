37d9ba7579bb38b769ad8cae0d76d38536ac78b7
ssufunny <wltnwhgdkgo@naver.com>
2022-02-08 17:50:14 +0900

    for문 변경 시도

--- test_dbInNodered.js
+++ test_dbInNodered.js
@@ -1,13 +1,14 @@
 // db에서 불러온 data를 활용하여 ui에 적용할 수 있도록 하는 코드
 
-sensorCount = msg.payload[0].LoRaNodeGpioRelayNo    // 센서 수(test.db에서 불러옴)
-
-if (sensorCount == 1)   // 센서 수가 1개라면 센서1의 정보만 보여줌
-    msg.payload={"group":{"hide":["자동개폐기_센서2", "자동개폐기_센서3", "자동개폐기_센서4"],"show":["자동개폐기_센서1"], "focus":true}}
-else if (sensorCount == 2)  // 센서 수가 2개라면 센서1과 센서2의 정보를 보여줌
-    msg.payload={"group":{"hide":["자동개폐기_센서3", "자동개폐기_센서4"],"show":["자동개폐기_센서1", "자동개폐기_센서2"], "focus":true}}
-else if (sensorCount == 3)  // 센서 수가 3개라면 센서1, 센서2, 센서3의 정보를 보여줌
-    msg.payload={"group":{"hide":["자동개폐기_센서4"],"show":["자동개폐기_센서1", "자동개폐기_센서2", "자동개폐기_센서3"], "focus":true}}
-else if (sensorCount == 4)  // 센서 수가 4개라면 센서1, 센서2, 센서3, 센서4의 정보를 보여줌
-    msg.payload={"group":{"show":["자동개폐기_센서1", "자동개폐기_센서2", "자동개폐기_센서3", "자동개폐기_센서4"], "focus":true}}
+var deviceCount = msg.payload[0].count // 각 장비 수
+let arr1 = [] // 출력확인용
+let arr2 =[] // 출력확인용
+let deviceCountAll =[]
+for (var i = 1; i <= deviceCount; i++) {
+    var deviceCountOver = deviceCount + i
+    deviceCountAll.push(i)
+    arr1.push(i) // 출력확인용
+    arr2.push(deviceCountOver) // 출력확인용
+}
+msg.count = arr1 + "/" + arr2 // 출력확인용
 return msg;
