6e6ad6ded0df433caddd43175028aba230b739b1
ssufunny <wltnwhgdkgo@naver.com>
2022-02-08 13:07:09 +0900

    장비 개수 구하는 sql문

---
+++ test_db2nodered.sql
@@ -0,0 +1,14 @@
+-- 코드 형식
+-- msg.topic = "sql문;"
+-- return msg;
+
+-- node-red와 db를 연결하여 원하는 정보를 불러오기 위한 코드 중 sql문만 작성
+
+-- 센서의 수
+select count(*) from test where OperationControlMode = 'S';
+-- 모터의 수
+select count(*) from test where ObjectType = 'M';
+-- FAN의 수
+select count(*) from test where FanDevDriverType is not null;
+-- Valve 수
+select count(*) from test where ObjectType = 'V';
