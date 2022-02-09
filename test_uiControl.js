cb0993733e9c2d2f2996498e5ffd45a175733349
ssufunny <wltnwhgdkgo@naver.com>
2022-02-09 11:09:30 +0900

    ui 동적 적용 코드(for문)

--- test_uiControl.js
+++ test_uiControl.js
@@ -1,4 +1,4 @@
-// db에서 불러온 data를 활용하여 ui에 적용할 수 있도록 하는 코드
+// db에서 불러온 data를 활용하여 장비 수에 맞게 ui가 동적으로 적용되도록 하는 코드 작성(for문 이용)
 
 var deviceCount = msg.payload[0].count // 각 장비 수
 let arr1 = [] // 출력확인용
