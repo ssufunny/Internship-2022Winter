1bd1c3e09ff479e29cbb74d564c24f0021204e4d
ssufunny <wltnwhgdkgo@naver.com>
2022-02-09 11:43:53 +0900

    defaultCount 설정

--- test_deviceCountIf.js
+++ test_deviceCountIf.js
@@ -1,4 +1,6 @@
 // db에서 불러온 data를 활용하여 장비 수에 맞게 ui가 동적으로 적용되도록 하는 코드 작성(if문 이용)
+
+var defaultCount = 8 // 장비 개수 default 값
 var deviceCount = msg.payload[0].count // 각 장비 수
 msg.count = deviceCount // 출력확인용
 
