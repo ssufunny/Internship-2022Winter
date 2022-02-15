e1185102228d30bdc5655b2cbdeeca7d2085f467
ssufunny <wltnwhgdkgo@naver.com>
2022-02-15 14:29:26 +0900

    주석 추가

--- test_setting.js
+++ test_setting.js
@@ -5,10 +5,10 @@ var dvcCount = msg.payload.length // 설정 장비 개수
 
 msg.payload = {
     "group": {
-        "show": [ 
+        "show": [   // 대시보드에 해당 그룹을 나타냄
             "Setting_설정입력" + dvcCount, "Setting_설정입력" + (dvcCount - 1), "Setting_설정입력" + (dvcCount - 2), "Setting_설정입력" + (dvcCount - 3), "Setting_설정입력" + (dvcCount - 4), "Setting_설정입력" + (dvcCount - 5)    // Setting 목록
         ], 
-        "hide": [ 
+        "hide": [   // 대시보드에서 해당 그룹을 숨김
             "Setting_설정입력" + (dvcCount + 1), "Setting_설정입력" + (dvcCount + 2), "Setting_설정입력" + (dvcCount + 3), "Setting_설정입력" + (dvcCount + 4), "Setting_설정입력" + (dvcCount + 5)    // Setting 목록
         ], 
         "focus":true
