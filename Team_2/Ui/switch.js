9a6e6326f3d76e0479e46327144e10601f7aded1
ssufunny <wltnwhgdkgo@naver.com>
2022-02-21 15:23:43 +0900

    주석 추가

--- Team_2/Ui/switch.js
+++ Team_2/Ui/switch.js
@@ -1,7 +1,10 @@
 // <개별스위치>
 // 스위치 종류 : F(FAN), V(Valve), A(AC)
+// 스위치 개수에 따라 그룹 번호 설정
+// 스위치 그룹은 한 그룹에 3개씩 6 그룹으로 총 개별스위치 개수 18개 제한
 
 var switchCount = msg.payload.length // 스위치 개수
+// 그룹 번호 설정
 if (switchCount <= 3) groupCount = 1
 else if (switchCount <= 6) groupCount = 2
 else if (switchCount <= 9) groupCount = 3
@@ -11,10 +14,10 @@ else if (switchCount <= 18) groupCount = 6
 
 msg.payload = {
     "group": {
-        "show": [ 
+        "show": [   // 대시보드에 해당 그룹을 나타냄
             "개별스위치_번호목록" + groupCount, "개별스위치_번호목록" + (groupCount - 1), "개별스위치_번호목록" + (groupCount - 2), "개별스위치_번호목록" + (groupCount - 3), "개별스위치_번호목록" + (groupCount - 4), "개별스위치_번호목록" + (groupCount - 5)    // 번호목록
         ], 
-        "hide": [ 
+        "hide": [   // 대시보드에서 해당 그룹을 숨김
             "개별스위치_번호목록" + (groupCount + 1), "개별스위치_번호목록" + (groupCount + 2), "개별스위치_번호목록" + (groupCount + 3), "개별스위치_번호목록" + (groupCount + 4), "개별스위치_번호목록" + (groupCount + 5)    // 번호목록
         ], 
         "focus":true
--- Team_2/Ui/switchName.js
+++ Team_2/Ui/switchName.js
@@ -2,7 +2,6 @@
 // 개별스위치 클릭 시 목록에 나타날 정보 중 장비이름에 들어갈 내용
 // 그룹이름은 FAN, 관수, AC 번호로 나타냄
 
-
 var dvcCount = msg.payload.length // 장비 개수
 var groupName = []  // 그룹이름 배열
 var fanCount = 0 // FAN 수
@@ -13,7 +12,7 @@ for (i = 0; i < dvcCount; i++) {
     var dvcType = msg.payload[i].ObjectType // 장비 타입(F or V or A)
     // FAN
     if (dvcType == 'F') {
-        fanCount += 1
+        fanCount += 1 // FAN 수
         groupName.push("FAN" + fanCount) // FAN + FAN 수
     }
     // 관수
@@ -23,7 +22,7 @@ for (i = 0; i < dvcCount; i++) {
     }
     // AC
     else if (dvcType == 'A') {
-        acCount += 1 // 밸브 수
+        acCount += 1 // AC 수
         groupName.push("AC" + acCount) // AC + AC 수
     }
 }
