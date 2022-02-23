db4ad087ee34a92a73bc2f61732d18a421461d9f
ssufunny <wltnwhgdkgo@naver.com>
2022-02-23 15:11:18 +0900

    2022.02.23 관수 그룹 크기, 위젯 크기 설명 주석 추가

--- Team_2/Ui/watering.js
+++ Team_2/Ui/watering.js
@@ -1,8 +1,8 @@
 // <관수>
 // [default] 관수의 개수는 최대 6개로 제한 : 6개 이하에서 동적인 화면 적용 가능 
 // 관수 개수 6개 초과 : 
-//  1) Node-RED Dashboard 관수 Tab에 그룹 추가 - 그룹이름 : 관수 + 관수번호
-//  2) 관수 flow에 관수 정보에 들어가는 Dashboad 위젯 추가 후 flow 설정 및 그룹 설정
+//  1) Node-RED Dashboard 관수 Tab에 그룹 추가 - 그룹이름 : 관수 + 관수번호, 그룹크기 : 20
+//  2) 관수 flow에 관수 정보에 들어가는 Dashboad 위젯 추가 후 flow 설정 및 그룹 설정 : switch, text 위젯 크기 : 4 x 1, button 위젯 크기 : 2 x 1
 //  3) watering.js 코드 수정 - 동적 화면 적용에 추가된 관수의 수만큼 show, hide 수정 
 //      ex) 관수 개수 8개일 때
 //          관수7, 관수8 그룹 생성 
