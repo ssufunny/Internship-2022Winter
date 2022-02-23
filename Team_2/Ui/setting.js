488233f27f4af95e8518593f151fbe5b15da8938
ssufunny <wltnwhgdkgo@naver.com>
2022-02-23 11:22:55 +0900

    2022.02.23 주석 디테일 수정

--- Team_2/Ui/setting.js
+++ Team_2/Ui/setting.js
@@ -1,10 +1,10 @@
 // <Setting>
 // [default] Setting 목록의 개수는 최대 6개로 제한 : 6개 이하에서 동적인 화면 적용 가능 
-// Setting 목록 개수 6개 이상 : 
+// Setting 목록 개수 6개 초과 : 
 //  1) Node-RED Dashboard Setting Tab에 그룹 추가 - 그룹이름 : 설정입력 + 목록번호 
 //  2) Setting flow에 Setting 정보에 들어가는 Dashboad 위젯 추가 후 flow 설정 및 그룹 설정
 //  3) setting.js 코드 수정 - 동적 화면 적용에 추가된 목록의 수만큼 show, hide 수정 
-//      ex) 목록 개수 8개 일 때
+//      ex) 목록 개수 8개일 때
 //          설정입력7, 설정입력8 그룹 생성 
 //          -> 위젯 추가 
 //          -> 코드 수정 : show에 "Setting_설정입력 + (dvcCount - 6)", "Setting_설정입력 + (dvcCount - 7)"  추가, 
